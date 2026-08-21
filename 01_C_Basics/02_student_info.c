db200af23db1ebcb28fb5daa666007e5bcea8f39
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-08-21 22:53:41 +0530

    Add program to print student information

---
+++ 01_C_Basics/02_student_info.c
@@ -0,0 +1,17 @@
+//preprocessor directive 
+#include<stdio.h>
+
+//main function 
+int main() {
+
+    //program description
+    printf("This program displays student information.\n\n");
+
+	//print student information
+	printf("Student Name: John Doe\n");
+	printf("Student ID: 12345\n");
+	printf("Grade: A\n");
+
+	//return statement 
+	return 0 ;
+}
---
+++ 01_C_Basics/03_data_types_and_variables.c
@@ -0,0 +1,21 @@
+//preprocessor directive 
+#include<stdio.h>
+
+//main function 
+int main() {
+
+	//Demonstration of data types & variables in C
+	int age = 25;
+	float height = 5.9f;
+	double weight = 70.5;
+	char grade = 'A';
+
+
+	printf("Age: %d\n", age);
+	printf("Height: %f\n", height);
+	printf("Weight: %lf\n", weight);
+	printf("Grade: %c\n", grade);
+
+	//return statement 
+	return 0 ;
+}
