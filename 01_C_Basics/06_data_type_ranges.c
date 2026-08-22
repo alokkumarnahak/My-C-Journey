7bd5d29eae13362db56a5df95079ee5e492aba72
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-08-22 16:37:40 +0530

    Add program to display ranges of data types in C

---
+++ 01_C_Basics/06_data_type_ranges.c
@@ -0,0 +1,35 @@
+//Ranges of data types in C
+
+//preprocessor directive
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+//main function
+int main()  {
+
+    //program description
+    printf("-: Ranges of data types in C :-\n\n");
+
+    //displaying the ranges of data types
+
+    //character data type ranges
+    printf("char: %d to %d\n", CHAR_MIN, CHAR_MAX);
+    printf("unsigned char: 0 to %u\n", UCHAR_MAX);
+
+    //integer data type ranges
+    printf("short: %d to %d\n", SHRT_MIN, SHRT_MAX);
+    printf("unsigned short: 0 to %u\n", USHRT_MAX);
+    printf("int: %d to %d\n", INT_MIN, INT_MAX);
+    printf("unsigned int: 0 to %u\n", UINT_MAX);
+    printf("long: %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("unsigned long: 0 to %lu\n", ULONG_MAX);
+
+    //floating point data type ranges
+    printf("float: %.10e to %.10e\n", FLT_MIN, FLT_MAX);
+    printf("double: %.10e to %.10e\n", DBL_MIN, DBL_MAX);
+    printf("long double: %.10Le to %.10Le\n", LDBL_MIN, LDBL_MAX);
+
+    return 0;
+    
+}
