e490b7b212e0ff4265295ff418380f1b2a11be28
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-08-29 21:33:12 +0530

    update positive_number.c
    This commit adds a simple C program that reads an integer and uses an if statement to check whether the value is positive. It includes user input, a positive-number message, and a fallback for non-positive values.

--- 03_Decision_Making/01_if/positive_number.c
+++ 03_Decision_Making/01_if/positive_number.c
@@ -1,1 +1,32 @@
+//Demonstration if statements
+//Program to find whether a number is positive or not
 
+//preprocessor directive
+#include<stdio.h>
+
+//main function
+int main()  {
+
+    //program description
+    printf("-: Program to find whether a number is positive or not :-\n\n");
+
+    //variable declaration
+    int number;
+
+    //input from user
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    //logic for chrecking a 
+    if(number > 0)  {
+        printf("You entered a positive number.");
+        return 0;   //if the number is positive then it terminates
+    }
+    
+    //if the if statement will not execute
+    printf("You entered a non-positive number.");
+    
+
+    //return statement
+    return 0;
+}
