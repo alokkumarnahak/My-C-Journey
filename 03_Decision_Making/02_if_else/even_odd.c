67d05437a5c87ff9eea2c22320b21d2e64991667
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-08-30 19:20:54 +0530

    Add even-odd C program and executable to 03_Decision_Making \ 01_if
    Add a simple C program demonstrating if-else to check whether an input number is even or odd (03_Decision_Making/02_if_else/even_odd.c). The program prompts for an integer, reads it with scanf, and prints "Even" or "Odd". The compiled executable even_odd.exe was also added.

--- 03_Decision_Making/02_if_else/even_odd.c
+++ 03_Decision_Making/02_if_else/even_odd.c
@@ -1,1 +1,33 @@
+// Demonstration if-else statements
+// Program to check even or odd
 
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main()
+{
+
+    // program description
+    printf("-: Program to check even or odd :-\n\n");
+
+    // variable declaration
+    int number;
+
+    // input from user
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // logic
+    if (number % 2 == 0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    // return statement
+    return 0;
+}
