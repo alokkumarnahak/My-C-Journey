c9c5e894882b6ce5b50f54383fffd07c791d479a
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-08-31 21:39:27 +0530

    Add greatest-of-three program and exe to 03_Decision_Making \ 03_else_if_ladder
    Replace greatest_3_numbers.c with a user-interactive C program that reads three integers and prints the greatest using nested if statements. Also add compiled greatest_3_numbers.exe to the repository for convenience.

--- 03_Decision_Making/03_else_if_ladder/greatest_3_numbers.c
+++ 03_Decision_Making/03_else_if_ladder/greatest_3_numbers.c
@@ -1,1 +1,45 @@
+// Demonstration nested-if statements
+// Find the greatest of three numbers
 
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main()
+{
+
+    // program description
+    printf("-: Find the greatest of three numbers :-\n\n");
+
+    // variable declaration
+    int num1, num2, num3;
+
+    // input from user
+    printf("Enter 1st number: ");
+    scanf("%d", &num1);
+
+    printf("Enter 2nd number: ");
+    scanf("%d", &num2);
+
+    printf("Enter 3rd number: ");
+    scanf("%d", &num3);
+
+    // logic to determine greatest of three numbers
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+            printf("%d is the greatest.", num1);
+        else
+            printf("%d is the greatest.", num3);
+    }
+    else
+    {
+        if (num2 > num3)
+            printf("%d is the greatest.", num2);
+        else
+            printf("%d is the greatest.", num3);
+    }
+
+    // return statement
+    return 0;
+}
