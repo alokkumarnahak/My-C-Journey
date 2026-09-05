c849ef2129dc1a8075ba9460007977d72f32d69e
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-05 21:54:59 +0530

    Add absolute value example using ternary operator to 03_Decision_Making / 07_conditional_ooperator
    Add absolute_value.c demonstrating the conditional (ternary) operator to compute and print the absolute value of an integer (reads input, evaluates (num<0)?-num:num, prints result). Also add the compiled Windows executable absolute_value.exe.

--- 03_Decision_Making/07_conditional_operator/absolute_value.c
+++ 03_Decision_Making/07_conditional_operator/absolute_value.c
@@ -1,1 +1,28 @@
+//Demonstration conditional operator
+//Find absolute value
+
+//preprocessor directive
+#include<stdio.h>
+
+//main function
+int main()  {
+
+    //program description
+    printf("-: Program to find absolute value :-\n\n");
+
+    //variable declaration
+    int num;
+    int abs_num;
+
+    //input from user
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    //logic
+    (num < 0)?(abs_num = -num,printf("Absolute value : %d", abs_num)):(abs_num = num,printf("Absolute value : %d",abs_num));
+
+    //return statement
+    return 0;
+}
+
 
