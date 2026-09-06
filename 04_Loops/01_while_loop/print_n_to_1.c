c3cf55cde3d6980063daaa02b951944cfa137a21
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-06 15:29:25 +0530

    Add while loop program: print n to 1 and exe to 04_Loops / 01_while_loop
    Add a simple C program demonstrating a while loop that prints positive numbers from n down to 1 (04_Loops/01_while_loop/print_n_to_1.c). Includes the compiled Windows executable print_n_to_1.exe for convenience.

---
+++ 04_Loops/01_while_loop/print_n_to_1.c
@@ -0,0 +1,29 @@
+//Demonstrate whole loop
+//Program to print positive numbers from n to 1
+
+//preprocessor directive
+#include<stdio.h>
+
+//main function
+int main()  {
+    int n;
+    int i;
+
+    //program description
+    printf("--- Program to print positive numbers from n to 1 ---\n\n");
+
+    //taking input from user
+    printf("Enter n(positive) : ");
+    scanf("%d",&n);
+
+    i  = n;
+
+    //logic
+    while(i > 0)    {
+        printf("%d\n",i);
+        i--;
+    }
+
+    //return statement
+    return 0;
+}
