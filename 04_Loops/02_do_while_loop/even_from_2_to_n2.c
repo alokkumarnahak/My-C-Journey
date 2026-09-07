10b922a6f9fd8bbe270d7563999183dc83631a19
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-07 19:23:21 +0530

    Add do-while example that prints even numbers between 2 to n to 04_Loops / 02_do_while_loop
    A a do-while loop program that takes a integer as input and prints the even numbers between 2 to n. Includes the compiled windows executable file.

---
+++ 04_Loops/02_do_while_loop/even_from_2_to_n2.c
@@ -0,0 +1,33 @@
+// Demonstrate do-while loop
+// Program to print even numbers from 2 to n
+
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main()
+{
+    int n;
+    int i = 2;
+
+    // program description
+    printf("--- Program to print even numbers from 2 to n ---\n\n");
+
+    // taking input from user
+    printf("Enter n(positive) : ");
+    if (scanf("%d", &n) != 1 || n <= 0)
+    {
+        printf("Invalid input !");
+        return 1;
+    }
+
+    // logic
+    do
+    {
+        printf("%d\n", i);
+        i += 2;
+    } while (i <= n);
+
+    // return statement
+    return 0;
+}
