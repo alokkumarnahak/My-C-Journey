ef4e1b2e6461d39706bfcb8c9c8f3db86b4b88b2
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-07 19:37:52 +0530

    Add do-while loop example to calculate sum of numbers from 1 to n to 04_Loops / 02_do_while_loop
    Add do-while loop program that takes a positive number as input and calculates the sum of numbers from 1 to n and prints it. Includes windows generated executable file.

---
+++ 04_Loops/02_do_while_loop/sum_from_1_to_n.c
@@ -0,0 +1,37 @@
+// Demonstrate do-while loop
+// Program to print sum from 1 to n
+
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main()
+{
+    int n;
+    int i = 1;
+    int sum = 0;
+
+    // program description
+    printf("--- Program to print sum from 1 to n ---\n\n");
+
+    // taking input from user
+    printf("Enter n(positive) : ");
+    if (scanf("%d", &n) != 1 || n <= 0)
+    {
+        printf("Invalid input !");
+        return 1;
+    }
+
+    // logic
+    do
+    {
+        sum += i;
+        i++;
+    } while (i <= n);
+
+    // printing sum
+    printf("Sum from 1 to %d is : %d\n", n, sum);
+
+    // return statement
+    return 0;
+}
