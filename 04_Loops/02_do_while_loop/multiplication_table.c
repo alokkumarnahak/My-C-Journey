7c3cb531e141541afc54a1faf92103ca48a2d371
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-07 19:33:10 +0530

    Add do-while loop example that prints multiplication table to 04_Loops / 02_do_while_loop
    Add a do-while loop example that takes a positive number as input and prints the multiplication table of it. Includes the windows compiled executable file.

---
+++ 04_Loops/02_do_while_loop/multiplication_table.c
@@ -0,0 +1,33 @@
+// Demonstrate while loop
+// Program to print multiplication table
+
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main()
+{
+    int n;
+    int i = 1;
+
+    // program description
+    printf("--- Multiplication Table ---\n\n");
+
+    // taking input from user
+    printf("Enter n(positive) : ");
+    if (scanf("%d", &n) != 1 || n <= 0)
+    {
+        printf("Invalid input !");
+        return 1;
+    }
+
+    // logic
+    do
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+        i++;
+    } while (i <= 10);
+
+    // return statement
+    return 0;
+}
