22d2cdb706f7374781e629619a4d459d8b10124c
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-07 18:42:25 +0530

    Add do-while loop example for print 1 to n numbers and exe to 04_Loops / 02_do_while_loop
    Add a C program that reads a positive integer and prints numbers from 1 to n using do-while loop. This also includes the generated windows executable for the sample program.

--- 04_Loops/02_do_while_loop/print_1_to_n2.c
+++ 04_Loops/02_do_while_loop/print_1_to_n2.c
@@ -1,1 +1,29 @@
+//Demonstrate do-while loop
+//Program to print positive numbers from 1 to n
+
+//preprocessor directive
+#include<stdio.h>
+
+//main function
+int main()  {
+    int n;
+    int i = 1;
+
+    //program description
+    printf("--- Program to print numbers from 1 to n ---\n\n");
+
+    //taking input from user
+    printf("Enter n(positive) : ");
+    scanf("%d",&n);
+
+    //logic
+    do{
+        printf("%d\n",i);
+        i++;
+    }
+    while(i <= n);
+
+    //return statement
+    return 0;
+}
 
