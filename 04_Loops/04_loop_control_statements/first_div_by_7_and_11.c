8d9f8dff6b9c753b95d4276f9373a6b724b22deb
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-09 20:06:01 +0530

    Add C program: first number divisible by 7 and 11 to 04_Loops / 04_loop_control_statements
    Add a simple C program demonstrating loop control statements. Prompts for N and searches from 2..N for the first integer divisible by both 7 and 11, prints it using break, and reports when none is found. Added file: 04_Loops/04_loop_control_statements/first_div_by_7_and_11.c.

--- 04_Loops/04_loop_control_statements/first_div_by_7_and_11.c
+++ 04_Loops/04_loop_control_statements/first_div_by_7_and_11.c
@@ -1,1 +1,37 @@
+//Demonstration of loop control statements in C
+//first number divisible by both 7 and 11
 
+//preprocessor directive
+#include <stdio.h>
+
+//main function
+int main()
+{
+    //program description
+    printf("--- Program to find first number divisible by both 7 and 11 ---\n\n");
+    
+    //variable declaration
+    int n;
+    int i;
+
+    //input from user
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    //logic
+    for (i = 2; i <= n; i++)
+    {
+        if (i % 7 == 0 && i % 11 == 0)
+        {
+            printf("First number divisible by both 7 and 11 : %d\n", i);
+            break;
+        }
+    }
+    if (i > n)
+    {
+        printf("No number found.\n");
+    }
+
+    //return statement
+    return 0;
+}
