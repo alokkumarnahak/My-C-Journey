c8e5806ff29b2aad6b59c8e3bafa4f6d11824b12
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-10 18:42:05 +0530

    Add repeated number triangle example to 04_Loops / 05_nested_loops
    Added a C example demonstrating nested loops to print a repeated number triangle. Updated the generated executable artifacts in the same folder to match the new program.

---
+++ 04_Loops/05_nested_loops/repeated_number_triangle.c
@@ -0,0 +1,35 @@
+//Demonstration of nested loops in C
+//repeated number triangle
+
+//preprocessor directive
+#include <stdio.h>
+
+//main function
+int main()
+{
+    //program description
+    printf("--- Program to print repeated number triangle ---\n\n");
+
+    //variable declaration
+    int n;
+    int i;
+    int j;
+
+    //input from user
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    //logic
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+
+        printf("\n");
+    }
+
+    //return statement
+    return 0;
+}
