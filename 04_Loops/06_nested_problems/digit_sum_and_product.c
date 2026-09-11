c853e8776526c6b880be70034bd04d1fd77a8778
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-11 21:56:56 +0530

    Add digit_sum_and_product.c and to 04_Loops / 06_number problems
    Add C program that takes a number and gives the sum and product of its digits. Includes windows generated executable.

--- 04_Loops/06_nested_problems/digit_sum_and_product.c
+++ 04_Loops/06_nested_problems/digit_sum_and_product.c
@@ -1,1 +1,40 @@
+//Demonstration of number problems using loops in C
+//digit sum and product
 
+//preprocessor directive
+#include <stdio.h>
+
+//main function
+int main()
+{
+    //program description
+    printf("--- Program to find sum and product of digits ---\n\n");
+
+    //variable declaration
+    int n;
+    int digit;
+    int sum = 0;
+    int product = 1;
+
+    //input from user
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    //logic
+    while (n > 0)
+    {
+        digit = n % 10;
+
+        sum = sum + digit;
+        product = product * digit;
+
+        n = n / 10;
+    }
+
+    //output
+    printf("Sum of digits = %d\n", sum);
+    printf("Product of digits = %d\n", product);
+
+    //return statement
+    return 0;
+}
