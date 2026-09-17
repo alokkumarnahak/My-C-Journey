85d02c331f813f7c9c150420a68f11c05f7fa70c
subhamkumar77 <subhamkumarbadatya647@gmail.com>
2026-09-17 21:43:28 +0530

    Update display_number.c
    Add a program that takes a number and displays it using a function named displayNumber.

--- 05_Functions/02_Parameters_and_Arguments/display_number.c
+++ 05_Functions/02_Parameters_and_Arguments/display_number.c
@@ -1,1 +1,26 @@
+// Demonstration of functions in C
+// function display number
 
+// preprocessor directive
+#include <stdio.h>
+
+// function declare and define
+void displayNumber(int n)    {
+    printf("%d\n",n);
+}
+
+// main function
+int main()
+{
+    // program description
+    printf("Demonstrate function\n\n");
+
+    //variable declaration
+    int num;
+
+    // function calling
+    displayNumber(7);
+
+    // return statement
+    return 0;
+}
