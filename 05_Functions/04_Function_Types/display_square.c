c70387f1738687cbd0f0a00e17aae9284a3dd72f
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-21 20:34:47 +0530

    Add display_square.c to 05_Functions\04_Function_Types
    Add a program that takes an integer as input and returns the square of it by using function.

---
+++ 05_Functions/04_Function_Types/display_square.c
@@ -0,0 +1,41 @@
+// Demonstration of types of functions
+// function with argument and no return value
+
+// preprocessor directive
+#include <stdio.h>
+
+// Function declaration
+void displaySquare(int n);
+
+// main function
+int main()
+{
+    // variable declaration
+    int number;
+
+    // user input
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // function call
+    displaySquare(number);
+
+    // return statement
+    return 0;
+}
+
+// Function definition
+void displaySquare(int n)S
+{
+    // variable declaration
+    int square;
+
+    // calculate square
+    square = n * n;
+
+    // output
+    printf("\n========== SQUARE ==========\n");
+    printf("Number : %d\n", n);
+    printf("Square : %d\n", square);
+    printf("============================\n");
+}
