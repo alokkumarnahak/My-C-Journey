b95be882ed632c4694289e619e719afcb9d03d32
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-21 20:32:04 +0530

    Add display_welcome.c to 05_Functions\04_Function_Types
    Add a program that prints welcome message for users by using function.

--- 05_Functions/04_Function_Types/display_welcome.c
+++ 05_Functions/04_Function_Types/display_welcome.c
@@ -1,1 +1,28 @@
+// Demonstration of types of functions
+// function with no argument and no return value
 
+// preprocessor directive
+#include <stdio.h>
+
+// Function declaration
+void displayWelcome();
+
+// main function
+int main()
+{
+    // function call
+    displayWelcome();
+
+    // return statement
+    return 0;
+}
+
+// Function definition
+void displayWelcome()
+{
+    printf("====================================\n");
+    printf("       WELCOME TO MY C JOURNEY\n");
+    printf("====================================\n");
+    printf("I am learning C programming.\n");
+    printf("I am improving my programming skills.\n");
+}
