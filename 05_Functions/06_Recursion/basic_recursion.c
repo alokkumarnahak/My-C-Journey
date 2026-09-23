923f5bdaa12bea8a6b26e0cb6e6a99dd587162f3
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-23 23:06:22 +0530

    Update basic_recursion.c
    Update program that takes a number n prints numbers from n to 1 in ascending order.

--- 05_Functions/06_Recursion/basic_recursion.c
+++ 05_Functions/06_Recursion/basic_recursion.c
@@ -1,1 +1,42 @@
+// Demonstration of recursion
+// function calling itself to count down
 
+// preprocessor directive
+#include <stdio.h>
+
+// Function declaration
+void countDown(int n);
+
+// main function
+int main()
+{
+    // variable declaration
+    int number;
+
+    // user input
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // function call
+    printf("\nCountdown:\n");
+    countDown(number);
+
+    // return statement
+    return 0;
+}
+
+// Function definition
+void countDown(int n)
+{
+    // base condition
+    if (n <= 0)
+    {
+        return;
+    }
+
+    // output
+    printf("%d\n", n);
+
+    // recursive function call
+    countDown(n - 1);
+}
