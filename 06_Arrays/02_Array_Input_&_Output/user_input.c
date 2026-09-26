0deb25296595919b16e8d48488d1fb3b11fcde5f
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-26 16:25:52 +0530

    Update user_input.c
    Update the C program so that it takes inputs from user along with its size.

--- 06_Arrays/02_Array_Input_&_Output/user_input.c
+++ 06_Arrays/02_Array_Input_&_Output/user_input.c
@@ -1,1 +1,34 @@
+// Demonstration of of arrays in C
+// Taking input from user
 
+// preprocessor directive
+#include<stdio.h>
+
+// main function
+int main()  {
+
+    // program description
+    printf("--- ARRAY INPUT ---\n\n");
+
+    // variable declaration
+    int size;
+
+    // size input from user
+    printf("Enter the array size: ");
+    scanf("%d", &size);
+
+    // array declaration
+    int arr[size];
+
+    // array elements input
+    printf("Enter array elements:\n");
+    
+    for(int i = 0; i < size; i++)   {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nArray input completed.\n");
+    
+    // return statement
+    return 0;
+}
