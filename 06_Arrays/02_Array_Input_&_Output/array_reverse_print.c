e54192dca994189850f692cfc0ba2ea631025b58
alokkumarnahak <alokkumarnahak4@gmail.com>
2026-09-26 16:40:01 +0530

    Add array_reverse_print.c to 06_Arrays\02_Array_Input_&_Output
    Add a C program that takes an array as input along with its size and prints it in reverse.

---
+++ 06_Arrays/02_Array_Input_&_Output/array_reverse_print.c
@@ -0,0 +1,61 @@
+// Demonstration of of arrays in C
+// Taking input from user and print output
+
+// preprocessor directive
+#include<stdio.h>
+
+// main function
+int main()  {
+
+    // program description
+    printf("--- ARRAY OUTPUT ---\n\n");
+
+    // variable declaration
+    int size;
+
+    // size input from user
+    printf("Enter the array size: ");
+    scanf("%d", &size);
+
+    // array declaration
+    int arr[size];
+
+    // array elements input
+    printf("Enter array elements:\n");
+    
+    for(int i = 0; i < size; i++)   {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array input completed.\n");
+
+    // print array
+    printf("\nArray:\n");
+    printf("{");
+    for(int i = 0; i < size; i++)   {
+        printf("%d", arr[i]);
+        if (i != size - 1)
+        {
+            printf(", ");
+        }
+        
+    }
+    printf("}\n");
+
+    // print array in reverse
+    printf("\nArray in reverse:\n");
+    printf("{");
+    for(int i = size - 1; i >= 0; i--)   {
+        printf("%d", arr[i]);
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        
+    }
+    printf("}\n");
+
+    printf("\nArray output completed.\n");
+
+    // return statement
+    return 0;
+}
